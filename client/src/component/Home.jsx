a0f704aa3b44c475dad1550ceea14d2499c9d65d
SonuSingh05 <sonusinghmourya143@gmail.com>
2024-10-27 14:40:41 +0530

    task31

---
+++ client/src/component/Home.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const [user, setUser] = useState([]);
+  const navigate= useNavigate()
+
+
+  useEffect(()=>{
+    if(!localStorage.getItem("token")){
+      navigate("/login") 
+    }
+    else{
+      navigate('/')
+    }
+  }, [])
+
+ 
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await axios("http://localhost:9000", {
+          headers:{
+            "Content-Type": "application/json",
+            token: localStorage.getItem("token")
+          }
+
+        })
+        
+        setUser(response.data);
+      };
+
+      fetchData();
+    }, []);
+  
+ 
+
+  return (
+    <div className="fetchDataDiv">
+      <h1>Logged Users</h1>
+      
+      {user.map((item, index) => {
+        return (
+          <div className="dataWrapper" key={index}>
+            <div>LoggedIn</div>
+            <div>{item.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+
+export default Home;
